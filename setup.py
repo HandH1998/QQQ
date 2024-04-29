0fc6e1dcdbdfa0cb9efb44f184897ba956fa72ce
HandH1998 <1335248067@qq.com>
2024-04-29 19:02:47 +0800

    add w4a8 compile

--- setup.py
+++ setup.py
@@ -7,16 +7,9 @@ setup(
         cpp_extension.CUDAExtension(
             name='QQQ._CUDA',
             sources=[
-                'csrc/int8gemm/cuda_utils.cc',
-                'csrc/int8gemm/cublasINT8MMWrapper.cc',
-                'csrc/int8gemm/cublasAlgoMap.cc',
-                'csrc/int8gemm/bindings.cpp',
+                'csrc/pybind.cpp',
+                'csrc/w4a8/w4a8_gemm.cu'
             ],
-            extra_link_args=['-lcublas_static', '-lcublasLt_static',
-                             '-lculibos', '-lcudart', '-lcudart_static',
-                             '-lrt', '-lpthread', '-ldl', '-L/usr/lib/x86_64-linux-gnu/'],
-            extra_compile_args={'cxx': ['-std=c++17', '-O3'],
-                                'nvcc': ['-O3', '-std=c++17', '-U__CUDA_NO_HALF_OPERATORS__', '-U__CUDA_NO_HALF_CONVERSIONS__', '-U__CUDA_NO_HALF2_OPERATORS__']},
         ),
     ],
     cmdclass={
